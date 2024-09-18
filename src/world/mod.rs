44dee5773ecc7d9ab77af0366f44b5cf2256be2a
Rancic <me@rancic.org>
2024-09-18 13:04:32 +0200

    refactor: Replace dummy background with another dummy background

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -10,7 +10,7 @@ pub struct WorldPlugin;
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(collisions::WorldCollisionPlugin)
-            .add_systems(OnExit(GameState::AssetLoading), (spawn_dummy_background,))
+            .add_systems(OnExit(GameState::AssetLoading), (spawn_dummy_backgrounds,))
             .add_systems(
                 PostUpdate,
                 update_camera_target.in_set(CameraSystem::TargetUpdate),
@@ -18,12 +18,22 @@ impl Plugin for WorldPlugin {
     }
 }
 
-fn spawn_dummy_background(mut commands: Commands, assets: Res<GameAssets>) {
+fn spawn_dummy_background(commands: &mut Commands, assets: &Res<GameAssets>, pos: Vec3) {
     commands.spawn(SpriteBundle {
         texture: assets.dummy_background.clone(),
-        transform: Transform::from_translation(Vec3::new(150.0, 0.0, 0.0)),
+        transform: Transform::from_translation(pos),
         ..default()
     });
+}
+
+fn spawn_dummy_backgrounds(mut commands: Commands, assets: Res<GameAssets>) {
+    let chunks = 32;
+    for i in -chunks..=chunks {
+        for j in -chunks..=chunks {
+            let pos = Vec2::new(i as f32 * 32.0, j as f32 * 32.0);
+            spawn_dummy_background(&mut commands, &assets, pos.extend(-10.0));
+        }
+    }
 }
 
 fn update_camera_target(
