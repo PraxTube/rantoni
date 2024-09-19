4ece467e27143dc3daaae657624e7ab6d1775be2
Rancic <me@rancic.org>
2024-09-19 20:00:34 +0200

    feat!: Use chain buffer time to allow for grace period when combo-ing

--- src/player/state.rs
+++ src/player/state.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 use bevy_trickfilm::prelude::*;
 
@@ -24,7 +26,7 @@ pub enum PlayerAttackState {
 }
 
 #[derive(Debug, Default, PartialEq, Clone, Copy)]
-pub enum ChainAttack {
+pub enum Attack {
     #[default]
     None,
     Light,
@@ -36,11 +38,28 @@ pub struct PlayerStateSystemSet;
 
 #[derive(Component, Default)]
 pub struct PlayerStateMachine {
-    state: PlayerState,
-    attack_state: PlayerAttackState,
-    previous_state: PlayerState,
-    chain_attack: ChainAttack,
     just_changed: bool,
+    state: PlayerState,
+    previous_state: PlayerState,
+    attack_handler: AttackHandler,
+}
+
+struct AttackHandler {
+    attack_state: PlayerAttackState,
+    chained_attack: Attack,
+    chainable: bool,
+    chain_buffer_timer: Timer,
+}
+
+impl Default for AttackHandler {
+    fn default() -> Self {
+        Self {
+            attack_state: PlayerAttackState::default(),
+            chained_attack: Attack::default(),
+            chainable: false,
+            chain_buffer_timer: Timer::from_seconds(0.3, TimerMode::Once),
+        }
+    }
 }
 
 impl PlayerStateMachine {
@@ -52,6 +71,10 @@ impl PlayerStateMachine {
 
     pub fn can_punch(&self) -> bool {
         self.can_run() || self.state == PlayerState::Attacking
+    }
+
+    fn previous_state(&self) -> PlayerState {
+        self.previous_state
     }
 
     pub fn state(&self) -> PlayerState {
@@ -70,11 +93,11 @@ impl PlayerStateMachine {
     }
 
     pub fn attack_state(&self) -> PlayerAttackState {
-        self.attack_state
+        self.attack_handler.attack_state
     }
 
     pub fn attack_state_eq(&self, attack_state: PlayerAttackState) -> bool {
-        self.state == PlayerState::Attacking && self.attack_state == attack_state
+        self.state == PlayerState::Attacking && self.attack_state() == attack_state
     }
 
     fn set_attack_state(&mut self, attack_state: PlayerAttackState) {
@@ -83,15 +106,17 @@ impl PlayerStateMachine {
             return;
         }
         self.set_state(PlayerState::Attacking);
-        self.attack_state = attack_state;
+        self.attack_handler.attack_state = attack_state;
+        self.attack_handler.chainable = true;
+        self.attack_handler.chain_buffer_timer.pause();
     }
 
-    pub fn chain_attack(&self) -> ChainAttack {
-        self.chain_attack
+    pub fn chained_attack(&self) -> Attack {
+        self.attack_handler.chained_attack
     }
 
-    fn set_chain_attack(&mut self, chain_attack: ChainAttack) {
-        self.chain_attack = chain_attack;
+    fn set_chained_attack(&mut self, attack: Attack) {
+        self.attack_handler.chained_attack = attack;
     }
 
     pub fn just_changed(&self) -> bool {
@@ -102,40 +127,88 @@ impl PlayerStateMachine {
         self.just_changed = just_changed;
     }
 
+    fn start_attack_chain_timer(&mut self) {
+        self.attack_handler.chain_buffer_timer.unpause();
+        self.attack_handler.chain_buffer_timer.reset();
+    }
+
+    fn handle_attack_chain_timer(&mut self, delta: Duration) {
+        self.attack_handler.chain_buffer_timer.tick(delta);
+        if self.attack_handler.chain_buffer_timer.just_finished() {
+            self.attack_handler.chainable = false;
+        }
+    }
+
+    fn default_attack(&mut self, attack: Attack) {
+        match attack {
+            Attack::None => {}
+            Attack::Light => self.set_attack_state(PlayerAttackState::Light1),
+            Attack::Heavy => self.set_attack_state(PlayerAttackState::Heavy1),
+        }
+    }
+
+    fn combo_attack(&self, attack: Attack) -> Option<PlayerAttackState> {
+        match self.attack_state() {
+            PlayerAttackState::Light1 => match attack {
+                Attack::None => panic!("should never happen!"),
+                Attack::Light => Some(PlayerAttackState::Light2),
+                Attack::Heavy => Some(PlayerAttackState::Heavy1),
+            },
+            PlayerAttackState::Light2 => None,
+            PlayerAttackState::Heavy1 => match attack {
+                Attack::None => panic!("should never happen!"),
+                Attack::Light => Some(PlayerAttackState::Light2),
+                Attack::Heavy => Some(PlayerAttackState::Heavy2),
+            },
+            PlayerAttackState::Heavy2 => None,
+        }
+    }
+
     fn transition_chain_attack(&mut self) {
-        if self.chain_attack() == ChainAttack::None {
+        if self.chained_attack() == Attack::None {
             self.set_state(PlayerState::Recovering);
             return;
         }
 
-        match self.attack_state() {
-            PlayerAttackState::Light1 => match self.chain_attack() {
-                ChainAttack::None => panic!("should never happen!"),
-                ChainAttack::Light => self.set_attack_state(PlayerAttackState::Light2),
-                ChainAttack::Heavy => self.set_attack_state(PlayerAttackState::Heavy1),
-            },
-            PlayerAttackState::Light2 => self.set_state(PlayerState::Recovering),
-            PlayerAttackState::Heavy1 => match self.chain_attack() {
-                ChainAttack::None => panic!("should never happen!"),
-                ChainAttack::Light => self.set_attack_state(PlayerAttackState::Light2),
-                ChainAttack::Heavy => self.set_attack_state(PlayerAttackState::Heavy2),
-            },
-            PlayerAttackState::Heavy2 => self.set_state(PlayerState::Recovering),
-        };
-        self.set_chain_attack(ChainAttack::None);
+        match self.combo_attack(self.chained_attack()) {
+            Some(attack_state) => self.set_attack_state(attack_state),
+            None => self.set_state(PlayerState::Recovering),
+        }
+        self.set_chained_attack(Attack::None);
+    }
+
+    fn transition_attack(&mut self, attack: Attack) {
+        if self.just_changed() {
+            return;
+        }
+        if !self.can_punch() {
+            return;
+        }
+
+        if self.state() == PlayerState::Attacking {
+            assert_ne!(attack, Attack::None);
+            self.set_chained_attack(attack);
+        } else if self.attack_handler.chainable {
+            match self.combo_attack(attack) {
+                Some(attack_state) => self.set_attack_state(attack_state),
+                None => self.default_attack(attack),
+            }
+        } else {
+            self.default_attack(attack);
+        }
     }
 
     pub fn state_animation(&self, assets: &Res<GameAssets>) -> (Handle<AnimationClip2D>, bool) {
         match self.state {
             PlayerState::Idling => (assets.player_animations[0].clone(), true),
             PlayerState::Running => (assets.player_animations[1].clone(), true),
-            PlayerState::Attacking => match self.attack_state {
+            PlayerState::Attacking => match self.attack_state() {
                 PlayerAttackState::Light1 => (assets.player_animations[2].clone(), false),
                 PlayerAttackState::Light2 => (assets.player_animations[4].clone(), false),
                 PlayerAttackState::Heavy1 => (assets.player_animations[7].clone(), false),
                 PlayerAttackState::Heavy2 => (assets.player_animations[9].clone(), false),
             },
-            PlayerState::Recovering => match self.attack_state {
+            PlayerState::Recovering => match self.attack_state() {
                 PlayerAttackState::Light1 => (assets.player_animations[3].clone(), false),
                 PlayerAttackState::Light2 => (assets.player_animations[5].clone(), false),
                 PlayerAttackState::Heavy1 => (assets.player_animations[8].clone(), false),
@@ -154,7 +227,7 @@ impl PlayerStateMachine {
                 error!("should never happen! run doesn't have any hitbox frames");
                 (0, 0)
             }
-            PlayerState::Attacking => match self.attack_state {
+            PlayerState::Attacking => match self.attack_state() {
                 PlayerAttackState::Light1 => (0, 1),
                 PlayerAttackState::Light2 => (0, 1),
                 PlayerAttackState::Heavy1 => (1, 2),
@@ -168,43 +241,16 @@ impl PlayerStateMachine {
     }
 }
 
-fn transition_punch_state(player_input: Res<PlayerInput>, mut q_player: Query<&mut Player>) {
-    let Ok(mut player) = q_player.get_single_mut() else {
-        return;
-    };
-    if player.state_machine.just_changed() {
-        return;
-    }
-
-    if !player.state_machine.can_punch() {
-        return;
-    }
-
-    if player_input.aim_direction != Vec2::ZERO {
-        player.aim_direction = player_input.aim_direction;
-    }
-
-    let chain_attack = if player_input.light_attack {
-        ChainAttack::Light
-    } else if player_input.heavy_attack {
-        ChainAttack::Heavy
-    } else {
-        return;
-    };
-
-    if player.state_machine.state() == PlayerState::Attacking {
-        assert_ne!(chain_attack, ChainAttack::None);
-        player.state_machine.set_chain_attack(chain_attack);
-    } else {
-        match chain_attack {
-            ChainAttack::None => {}
-            ChainAttack::Light => player
-                .state_machine
-                .set_attack_state(PlayerAttackState::Light1),
-            ChainAttack::Heavy => player
-                .state_machine
-                .set_attack_state(PlayerAttackState::Heavy1),
-        }
+fn transition_attack_state(player_input: Res<PlayerInput>, mut q_players: Query<&mut Player>) {
+    for mut player in &mut q_players {
+        let attack = if player_input.light_attack {
+            Attack::Light
+        } else if player_input.heavy_attack {
+            Attack::Heavy
+        } else {
+            continue;
+        };
+        player.state_machine.transition_attack(attack);
     }
 }
 
@@ -262,6 +308,38 @@ fn reset_just_changed(mut q_player: Query<&mut Player>) {
     player.state_machine.set_just_changed(false);
 }
 
+fn update_aim_direction(player_input: Res<PlayerInput>, mut q_players: Query<&mut Player>) {
+    for mut player in &mut q_players {
+        if player_input.aim_direction == Vec2::ZERO {
+            continue;
+        }
+
+        if player.state_machine.just_changed() {
+            player.aim_direction = player_input.aim_direction;
+        }
+    }
+}
+
+fn start_attack_chain_timer(mut q_players: Query<&mut Player>) {
+    for mut player in &mut q_players {
+        if !player.state_machine.just_changed() {
+            continue;
+        }
+
+        if player.state_machine.previous_state() == PlayerState::Attacking
+            && player.state_machine.state() != PlayerState::Attacking
+        {
+            player.state_machine.start_attack_chain_timer();
+        }
+    }
+}
+
+fn handle_attack_chain_timer(time: Res<Time>, mut q_players: Query<&mut Player>) {
+    for mut player in &mut q_players {
+        player.state_machine.handle_attack_chain_timer(time.delta());
+    }
+}
+
 pub struct PlayerStatePlugin;
 
 impl Plugin for PlayerStatePlugin {
@@ -270,12 +348,22 @@ impl Plugin for PlayerStatePlugin {
             Update,
             (
                 reset_just_changed,
-                transition_punch_state,
+                transition_attack_state,
                 transition_idle_state,
                 transition_run_state,
             )
                 .chain()
                 .in_set(PlayerStateSystemSet),
+        )
+        .add_systems(
+            Update,
+            (
+                update_aim_direction,
+                start_attack_chain_timer,
+                handle_attack_chain_timer,
+            )
+                .chain()
+                .after(PlayerStateSystemSet),
         );
     }
 }
