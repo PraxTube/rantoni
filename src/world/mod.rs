eafa136eef831164095bdcc1e47e5966e1da0744
Rancic <me@rancic.org>
2024-12-13 10:03:49 +0100

    fix: Toggle debug on start

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -3,12 +3,15 @@ pub mod collisions;
 mod camera;
 mod map;
 
+use bevy_rancic::prelude::DebugState;
 pub use map::{
     a_star, CachedEnemy, CachedLevelData, DespawnLevelSystemSet, LevelChanged, PathfindingSource,
     PathfindingTarget, WorldSpatialData,
 };
 
 use bevy::prelude::*;
+
+use crate::GameState;
 
 pub struct WorldPlugin;
 
@@ -18,9 +21,14 @@ impl Plugin for WorldPlugin {
             collisions::WorldCollisionPlugin,
             map::WorldMapPlugin,
             camera::CameraPlugin,
-        ));
+        ))
+        .add_systems(OnExit(GameState::AssetLoading), toggle_debug);
     }
 }
 
 #[derive(Component)]
 pub struct WorldEntity;
+
+fn toggle_debug(mut debug_state: ResMut<DebugState>) {
+    **debug_state = !**debug_state;
+}
